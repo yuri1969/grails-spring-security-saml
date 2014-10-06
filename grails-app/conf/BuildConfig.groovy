5d575a079ec8fe16b5ea7ac3623737e7581d6f31
Jeff Beck <beckje01@gmail.com>
2014-10-06 16:31:28 -0500

    Switch to maven depenency resolver.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,36 +2,51 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
-grails.release.scm.enabled = false
-
+////grails.release.scm.enabled = false
+//
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
-    inherits('global') {
-    }
+    inherits 'global'
     log 'warn'
 
     repositories {
         grailsCentral()
+        mavenLocal()
         mavenCentral()
     }
 
     dependencies {
 
+        String springSecurityVersion = '3.2.4.RELEASE'
+
         compile 'org.springframework.security.extensions:spring-security-saml2-core:1.0.0.RELEASE'
+        compile "org.springframework.security:spring-security-web:$springSecurityVersion", {
+            excludes 'aopalliance', 'commons-codec', 'commons-logging', 'fest-assert', 'groovy', 'hsqldb',
+                    'jcl-over-slf4j', 'junit', 'logback-classic', 'mockito-core', 'powermock-api-mockito',
+                    'powermock-api-support', 'powermock-core', 'powermock-module-junit4',
+                    'powermock-module-junit4-common', 'powermock-reflect', 'spock-core', 'spring-beans',
+                    'spring-context', 'spring-core', 'spring-expression', 'spring-jdbc',
+                    'spring-security-core', 'spring-test', 'spring-tx', 'spring-web', 'spring-webmvc',
+                    'tomcat-servlet-api'
+        }
     }
 
     plugins {
 
         test ":code-coverage:1.2.5"
 
+        compile(":spring-security-core:2.0-RC4")
+
         compile(":build-test-data:2.2.1",
-                ":hibernate:3.6.10.17",
-                ":spring-security-core:2.0-RC4",
-                ":svn:1.0.2") {
+                ":hibernate:3.6.10.17") {
             export = false
         }
 
-        build(":tomcat:7.0.54",
-                ":release:3.0.1") {
+        build(":tomcat:7.0.54") {
+            export = false
+        }
+
+        build ':release:3.0.1', ':rest-client-builder:2.0.1', {
             export = false
         }
     }
